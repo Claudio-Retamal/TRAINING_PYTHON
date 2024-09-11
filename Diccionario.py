2cdd5ed35053296add8150c798571f2852b8b5d8
claudio996 <claudioantonio.retamal@gmail.com>
2024-09-10 22:05:11 -0300

    Working in list

---
+++ Diccionario.py
@@ -0,0 +1,35 @@
+""" lista = ['a', 1, 2, '3']
+print(len(lista))
+#
+
+
+# i = 0 """
+# i = 0
+
+# suma  = 0
+# lista2 = [1,2,3,4,5]
+# while i < len(lista2):
+#     suma  = suma +  lista2[i]
+#     i+= 1
+#     print(suma)
+   
+""" list_empty = []
+
+for i in range(0,4):
+    
+    data = input('into new value to list_apend')
+    list_empty.append(data)
+    print(list_empty) """
+
+# list_empty = []
+# i = 3  
+# while i != 4: 
+#     data = int(input('into new value to list_apend \n'))
+#     if(data == 0):
+#         print('the size of list is : ', len(list_empty)) 
+#         i = 4
+#     else:
+#         print('new add value')
+#         list_empty.append(data)
+    
+    
